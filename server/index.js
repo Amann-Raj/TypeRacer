e109a4e5c6ef138331fd1e48d4b7cda7cd829449
Amann-Raj <amanraj1227@gmail.com>
2026-03-26 16:01:05 +0530

    register and login endpoints wih jwt auth middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import passageRoutes from './routes/passageRoutes.js'
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config()
 
@@ -13,6 +14,7 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 app.use('/api/passages', passageRoutes)
+app.use('/api/auth', authRoutes)
 
 app.get('/api/health', (req, res) => {
     res.json({ status: 'ok', message: 'Server is running' })
